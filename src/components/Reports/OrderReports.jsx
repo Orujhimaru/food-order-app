1a08a0e6c2c9fb877597ad3a7a8fbfd263eec852
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-05-18 15:51:00 +0300

    added a data file

--- src/components/Reports/OrderReports.jsx
+++ src/components/Reports/OrderReports.jsx
@@ -4,91 +4,9 @@ import { useEffect } from "react";
 import "./OrderReport.scss";
 import DropdownFilter from "./DropdownFilter";
 import ReportElement from "./ReportElement";
+import { arrOfOrders } from "./data";
 
 // mock data
-let arrOfOrders = [
-  {
-    id: "1234dfs141",
-    userName: "Eren Jeager",
-    img: "/profile-picture.png",
-    orderName: "Spicy seasoned seafood noodles ",
-    price: "$125",
-    status: "completed",
-  },
-  {
-    id: "12sf341",
-    userName: "Hangi",
-    img: "/profile-picture.png",
-    orderName: "Raw meat",
-    price: "$12532",
-    status: "pending",
-  },
-  {
-    id: "32134141",
-    userName: "Levi",
-    img: "/profile-picture.png",
-    orderName: "Potato",
-    price: "$32",
-    status: "preparing",
-  },
-  {
-    id: "123as434ff141",
-    userName: "Eren Jeager",
-    img: "/profile-picture.png",
-    orderName: "Spicy seasoned seafood noodles ",
-    price: "$125",
-    status: "completed",
-  },
-  {
-    id: "1234rfgadsv141",
-    userName: "Hangi",
-    img: "/profile-picture.png",
-    orderName: "Raw meat",
-    price: "$12532",
-    status: "pending",
-  },
-  {
-    id: "1234s34f141",
-    userName: "Levi",
-    img: "/profile-picture.png",
-    orderName: "Potato",
-    price: "$32",
-    status: "preparing",
-  },
-  {
-    id: "12343gdfs4ff141",
-    userName: "Eren Jeager",
-    img: "/profile-picture.png",
-    orderName: "Spicy seasoned seafood noodles ",
-    price: "$125",
-    status: "completed",
-  },
-  {
-    id: "1234rfgsvfsdf141",
-    userName: "Hangi",
-    img: "/profile-picture.png",
-    orderName: "Raw meat",
-    price: "$12532",
-    status: "pending",
-  },
-  {
-    id: "123434ff1sags41",
-    userName: "Eren Jeager",
-    img: "/profile-picture.png",
-    orderName: "Spicy seasoned seafood noodles ",
-    price: "$125",
-    status: "completed",
-  },
-  {
-    id: "1234rgsfdffgsv141",
-    userName: "Hangi",
-    img: "/profile-picture.png",
-    orderName: "Raw meat",
-    price: "$12532",
-    status: "pending",
-  },
-];
-
 function returnElements(array) {
   let arr = array.map((order) => {
     return (
@@ -110,9 +28,9 @@ function OrderReports() {
   const [elements, setElements] = React.useState(returnElements(arrOfOrders));
   // MAKE DROPDOWN VISIBLE state
   const [dropDown, setDropDown] = React.useState(false);
-
   // Q1: Dropdown kismi sadece Filter buttonuna tikladigimda kapaniyor,
   // baska bir yere tikladigimda kapanmasini nasil yapa bilirim ?
+  // Q2: USE REF NEREDE KULLANILIR ?
 
   // CHANGE category STATE { all, preparing, pending, completed}
   const [category, setCategory] = React.useState("all");
@@ -128,7 +46,7 @@ function OrderReports() {
   // CHANGING THE LIST OF ORDERS BY THEIR STATUS
   useEffect(() => {
     console.log("run");
-    // q2: IKi kere run printlemesinin sebebi StrictMode mu ?
+    // q3: IKi kere run printlemesinin sebebi StrictMode mu ?
     if (category === "all") {
       setElements(returnElements(arrOfOrders));
     } else {
