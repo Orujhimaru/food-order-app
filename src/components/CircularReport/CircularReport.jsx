b26df31d0e045185f6c70475a615c1292820789f
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-05-20 12:23:00 +0300

    [MA-04] Initial setup

---
+++ src/components/CircularReport/CircularReport.jsx
@@ -0,0 +1,3 @@
+export default function CircularReport() {
+  return <div>happy</div>;
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
 import "./HomePage.scss";
+import CircularReport from "@/components/CircularReport/CircularReport.jsx";
 
 function HomePage() {
   return (
     <div>
       <Sidebar />
       <div className="mainRoot">
-        <h1 style={{ height: "1600px" }}>Homepage example</h1>
+        <h1>Homepage example</h1>
+        <CircularReport />
       </div>
     </div>
   );
