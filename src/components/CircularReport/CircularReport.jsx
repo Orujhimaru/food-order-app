e2f4b18586b24539c6fd20839e05910b16386edf
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-05-23 16:41:28 +0300

    small css changes :(

--- src/components/CircularReport/CircularReport.jsx
+++ src/components/CircularReport/CircularReport.jsx
@@ -1,147 +1,37 @@
-import { useEffect, useState, useRef } from "react";
-import ApexCharts from "apexcharts";
 import "./CircularReport.scss";
-import Chart from "react-apexcharts";
+import { useEffect, useState, useRef } from "react";
+import CircularReportAnimation from "./CircularReportAnimation/CircularReportAnimation";
 
 export default function CircularReport({ delivery, toGo, dineIn }) {
-  // let [total, setTotal] = useState(dineIn + delivery + toGo);
-  // state is not necessary here ?
-  let total = dineIn + delivery + toGo;
-
-  let series1 = [(delivery * 100) / total];
-  let series2 = [(toGo * 100) / total];
-  let series3 = [(dineIn * 100) / total];
-
-  let options1 = {
-    plotOptions: {
-      radialBar: {
-        track: {
-          background: "#353440",
-        },
-        hollow: {
-          margin: 15,
-          size: "70%",
-          // background: "#1F1D2B",
-        },
-
-        dataLabels: {
-          showOn: "always",
-          name: {
-            show: false,
-          },
-
-          value: {
-            show: false,
-          },
-        },
-      },
-    },
-
-    fill: {
-      colors: "#65B0F6",
-    },
-
-    stroke: {
-      lineCap: "butt",
-      width: 1,
-
-      // HOW DO I SET IT TO ROUND AT THE END AND BUTT AT THE START ????
-    },
-  };
-
-  let options2 = {
-    plotOptions: {
-      radialBar: {
-        hollow: {
-          margin: 15,
-          size: "70%",
-        },
-        track: {
-          background: "#2A2836",
-        },
-
-        dataLabels: {
-          showOn: "always",
-          name: {
-            show: false,
-          },
-
-          value: {
-            show: false,
-          },
-        },
-      },
-    },
-    fill: {
-      colors: "#FFB572",
-    },
-    stroke: {
-      lineCap: "butt",
-      width: 1,
-    },
-  };
-
-  let options3 = {
-    plotOptions: {
-      radialBar: {
-        hollow: {
-          margin: 15,
-          size: "70%",
-        },
-        track: {
-          background: "#353440",
-        },
-
-        dataLabels: {
-          showOn: "always",
-          name: {
-            show: false,
-          },
-
-          value: {
-            show: false,
-          },
-        },
-      },
-    },
-    fill: {
-      colors: "#FF6996",
-    },
-    stroke: {
-      lineCap: "butt",
-      width: 1,
-    },
-  };
-
   return (
-    <div className="donut">
-      <div className="inner-4"></div>
-      <div className="inner-1">
-        <Chart
-          options={options1}
-          series={series1}
-          type="radialBar"
-          width="356"
-          // width="680"
-        />
-      </div>
-      <div className="inner-2">
-        <Chart
-          options={options2}
-          series={series2}
-          type="radialBar"
-          width="318"
-          // width="522"
-        />
-      </div>
-      <div className="inner-3">
-        <Chart
-          options={options3}
-          series={series3}
-          type="radialBar"
-          width="285"
-          // width="410"
-        />
+    <div className="circular-report-container">
+      <CircularReportAnimation
+        delivery={delivery}
+        toGo={toGo}
+        dineIn={dineIn}
+      />
+      <div className="order-stats">
+        <div className="order-stat">
+          <div className="stat-indicator-pink"></div>
+          <h3>
+            Dine In
+            <br /> {dineIn} customers
+          </h3>
+        </div>
+        <div className="order-stat">
+          <div className="stat-indicator-blue"></div>
+          <h3>
+            To Go
+            <br /> {toGo} customers
+          </h3>
+        </div>
+        <div className="order-stat">
+          <div className="stat-indicator-yellow"></div>
+          <h3>
+            Delivery
+            <br /> {delivery} customers
+          </h3>
+        </div>
       </div>
     </div>
   );
