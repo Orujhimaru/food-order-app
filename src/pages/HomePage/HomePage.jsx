5a78635f9e68011c79185fb8a53989e278b8ff1f
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-05-30 14:08:22 +0300

    .

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,11 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
 import "./HomePage.scss";
-import OrderCard from "../../components/OrderCard/OrderCard";
 
 function HomePage() {
   return (
     <div>
       <Sidebar />
-      <div className="mainRoot">
-        <h1 style={{ height: "100px" }}>Homepage example</h1>
-      </div>
+      <div className="mainRoot"></div>
     </div>
   );
 }
