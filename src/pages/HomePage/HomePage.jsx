4a2aad080fee06f1f19420534d30d73d26fc09a0
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-05-30 13:56:50 +0300

    [MA-4] .removed homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,11 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
 import "./HomePage.scss";
-import CircularReport from "@/components/CircularReport/CircularReport.jsx";
 
 function HomePage() {
   return (
     <div>
       <Sidebar />
-      <div className="mainRoot">
-        <h1 style={{ display: "block" }}>Homepage example</h1>
-        <CircularReport delivery={1560} toGo={250} dineIn={1150} />
-      </div>
+      <div className="mainRoot"></div>
     </div>
   );
 }
