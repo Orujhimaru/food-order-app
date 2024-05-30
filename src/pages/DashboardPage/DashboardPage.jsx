7ab4ea3afbf02c94b3f6fb9a3e03eba7713955a9
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-05-30 12:56:07 +0300

    routes changed, scss variables added, Dashboard page added

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,16 +1,17 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
-import "./HomePage.scss";
+import "./DashboardPage.scss";
 import OrderCard from "../../components/OrderCard/OrderCard";
 
-function HomePage() {
+function DashboardPage() {
   return (
     <div>
       <Sidebar />
       <div className="mainRoot">
-        <h1 style={{ height: "100px" }}>Homepage example</h1>
+        {/* <h1 style={{ height: "100px" }}>Homepage example</h1> */}
+        <OrderCard />
       </div>
     </div>
   );
 }
 
-export default HomePage;
+export default DashboardPage;
