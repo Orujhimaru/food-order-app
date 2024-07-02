67142c685a74fd49373703a690bffd4dbde50e75
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-07-03 00:22:25 +0300

    Navbar styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,20 @@
-
-
-function NavBar(){
-
-
-    return (
-        <nav>
-<ul class="nav-list">
-  <li class="active">
-    <a href="index.html" aria-current="page">Home</a>
-  </li>
-  <nav>
-    )
+function returnOrderReportElements() {
+  return <div></div>;
 }
 
+function NavBar() {
+  return (
+    <nav>
+      <ul class="nav-list">
+        <li class="active">
+          <a href="index.html" aria-current="page">
+            Home
+          </a>
+        </li>
+      </ul>
+      renderNavBar()
+    </nav>
+  );
+}
+
+export default NavBar;
