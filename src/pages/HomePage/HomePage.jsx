e79561d6204cf58ebbfeaae65b7906062e089a31
slhoz <lgsalih@gmail.com>
2024-07-03 09:58:10 +0300

    filtrleme sistemi yapıldı

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,10 +9,6 @@ import { useState, useEffect } from "react";
 import { foods, categories } from "@/db/food";
 import { useAuth } from "@/components/AuthProvider";
 
-
-import SettingsProductManagement from "../../components/SettingsProductManagement/SettingsProductManagement";
-
-
 function HomePage() {
   const { fireStoreUser } = useAuth();
 
@@ -21,22 +17,11 @@ function HomePage() {
   return (
     <div>
       <Sidebar />
-
       <div className="mainRoot">
         <Header />
-
-
-        <h1 style={{ height: "1600px" }}>Homepage example</h1>
-
-        <CategoryTabs setCategory={setCategory} category={category} />
-
-        <CategoryTabs
-          categories={categories}
-          setCategory={setCategory}
-          category={category}
-        />
-        <SettingsProductManagement />
-
+        <div className="dishesMen">
+          <DishesMenu />
+        </div>
       </div>
     </div>
   );
