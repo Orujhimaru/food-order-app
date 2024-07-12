ad638365aeabcd6f25d9d9dcb08caa668e0ac481
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-07-12 17:39:05 +0300

    [MA-14] Works with only 1 state

--- src/components/SettingsProductManagement/SettingsProductManagement.jsx
+++ src/components/SettingsProductManagement/SettingsProductManagement.jsx
@@ -7,70 +7,57 @@ import { useState, useRef } from "react";
 export default function SettingsProductManagement() {
   const [category, setCategory] = useState("all");
   const [foodList, setFoodList] = useState(foods); // yemekler
-  const [editState, setEditState] = useState(false);
-  const [editedFoodItem, setEditedFoodItem] = useState({});
+  const [editedFoodItem, setEditedFoodItem] = useState();
 
-  const inputRef1 = useRef();
-  const inputRef2 = useRef();
-
-  function setEditMode(dish) {
-    if (isObjectEmpty(editedFoodItem)) {
-      setEditState(true);
-      setEditedFoodItem(dish);
-    } else {
-      setEditState(false);
-      setEditedFoodItem({});
-    }
-  }
-
-  function isObjectEmpty(objectName) {
-    return Object.keys(objectName).length === 0;
-  }
-
-  function editFoodList(newObj) {
-    if (newObj.description.length === 0 || newObj.price.length === 0) {
+  function editFoodList() {
+    // Updates the list after checking input values.
+    if (
+      editedFoodItem.description.length === 0 ||
+      editedFoodItem.price.length === 0
+    ) {
       return;
     }
     setFoodList(
       foodList.map((dish) => {
-        return dish.id === newObj.id ? newObj : dish;
+        return dish.id === editedFoodItem.id ? editedFoodItem : dish;
       })
     );
+    setEditedFoodItem({});
   }
+
+  const handleInputChange = (event) => {
+    setEditedFoodItem({
+      ...editedFoodItem,
+      [event.target.name]: event.target.value,
+    });
+    event.preventDefault();
+  };
 
   function renderDishes(dishes) {
     console.log(dishes);
-    console.log(foodList);
     let arr = dishes?.map((dish) => {
       return (
         (dish.category.key === category || category === "all") && (
           <div className="dish" key={dish.id}>
             <img src={dish.image}></img>
-            {editState === true && editedFoodItem.id === dish.id ? (
+            {editedFoodItem && editedFoodItem.id === dish.id ? (
               <>
                 <input
+                  name="description"
                   type="text"
-                  // onChange={(event) => setEditedField(event.target.value)}
-                  ref={inputRef1}
+                  onChange={(event) => handleInputChange(event)}
                   placeholder={dish.description}
                 ></input>
                 <input
+                  name="price"
                   type="text"
-                  // onChange={(event) => setEditedField2(event.target.value)}
-                  // value={editedField2}
-                  ref={inputRef2}
+                  onChange={(event) => handleInputChange(event)}
                   placeholder={dish.price}
                 ></input>
                 <button
                   className="edit-button-save"
                   onClick={() => {
-                    const newObj = {
-                      ...dish,
-                      description: inputRef1.current.value,
-                      price: inputRef2.current.value,
-                    };
-                    editFoodList(newObj);
-                    setEditMode(newObj);
+                    editFoodList();
                   }}
                 >
                   <div className="button-elements-container">
@@ -86,7 +73,7 @@ export default function SettingsProductManagement() {
                 <button
                   className="edit-button"
                   onClick={() => {
-                    setEditMode(dish);
+                    setEditedFoodItem(dish);
                   }}
                 >
                   <div className="button-elements-container">
